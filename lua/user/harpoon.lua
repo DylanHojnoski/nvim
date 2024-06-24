ba9cc77dd81db462bc8ebeb5284e2ebce626c156
Dylan Hojnoski <dylanhoj@gmail.com>
2024-06-24 17:26:30 -0400

    test

--- lua/user/harpoon.lua
+++ lua/user/harpoon.lua
@@ -1,10 +1,25 @@
+local harpoon = require("harpoon")
 
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+--
+local settings = {
+    save_on_toggle = true,
+    sync_on_ui_close = true,
+    key = function()
+        return vim.loop.cwd()
+    end,
+}
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-2>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-3>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-4>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<leader>u", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>i", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>o", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>p", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next b ffers stoked within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,52 +1,58 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.g.mapleader = " "
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	'nvim-lua/plenary.nvim',
-	'nvim-treesitter/nvim-treesitter',
-	{"nvim-treesitter/nvim-treesitter", lazy = false, build = ":TSUpdate"},
-	'ThePrimeagen/harpoon',
-	'jiaoshijie/undotree',
-	'tpope/vim-fugitive',
-	'nvim-telescope/telescope.nvim',
+    'nvim-lua/plenary.nvim',
+    'nvimtools/none-ls.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    {"nvim-treesitter/nvim-treesitter", lazy = false, build = ":TSUpdate"},
+    {"ThePrimeagen/harpoon", branch = "harpoon2"},
+    'jiaoshijie/undotree',
+    'tpope/vim-fugitive',
+    'nvim-telescope/telescope.nvim',
 
     -- colorschemes
-	'sainnhe/everforest',
-    'ellisonleao/gruvbox.nvim',
-	'VonHeikemen/lsp-zero.nvim',
-	branch = 'v1.x',
-		-- LSP Support
-	{'neovim/nvim-lspconfig'},             -- Required
-	{'williamboman/mason.nvim'},           -- Optional
-	{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    'sainnhe/everforest',
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {'williamboman/mason.nvim',
+    opts = {
+        registries = {
+            'github:nvim-java/mason-registry',
+            'github:mason-org/mason-registry',
+        },
+    },
+},           -- Optional
+{'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-	-- Autocompletion
-	{'hrsh7th/nvim-cmp'},         -- Required
-	{'hrsh7th/cmp-nvim-lsp'},     -- Required
-	{'hrsh7th/cmp-buffer'},       -- Optional
-	{'hrsh7th/cmp-path'},         -- Optional
-	{'saadparwaiz1/cmp_luasnip'}, -- Optional
-	{'hrsh7th/cmp-nvim-lua'},     -- Optional
-    "windwp/nvim-autopairs",
+-- Autocompletion
+{'hrsh7th/nvim-cmp'},         -- Required
+{'hrsh7th/cmp-nvim-lsp'},     -- Required
+{'hrsh7th/cmp-buffer'},       -- Optional
+{'hrsh7th/cmp-path'},         -- Optional
+{'saadparwaiz1/cmp_luasnip'}, -- Optional
+{'hrsh7th/cmp-nvim-lua'},     -- Optional
+"windwp/nvim-autopairs",
 
-	-- Snippets
-	{'L3MON4D3/LuaSnip'},             -- Required
-	{'rafamadriz/friendly-snippets'}, -- Optional
+-- Snippets
+{'L3MON4D3/LuaSnip'},             -- Required
+{'rafamadriz/friendly-snippets'}, -- Optional
 
     'ahmedkhalf/project.nvim',
     'nvim-lualine/lualine.nvim',
-    -- "epwalsh/obsidian.nvim",
 }
 
 local opts = {}
