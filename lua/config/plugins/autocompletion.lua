8b348f76e4fab50440dfbd0d4da5795a000c7442
Dylan Hojnoski <dylanhoj@gmail.com>
2025-06-23 19:09:20 -0400

    Removed nerd icons

--- lua/config/plugins/autocompletion.lua
+++ lua/config/plugins/autocompletion.lua
@@ -26,12 +26,6 @@ return {
         ['<CR>'] = {'accept', 'fallback'},
     },
 
-    appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
-    },
-
     -- (Default) Only show the documentation popup when manually triggered
     completion = { documentation = { auto_show = false }, },
 
